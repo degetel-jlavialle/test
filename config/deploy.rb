50747bc77b400fb36f9d014dd6515c4b3b8cc874
degetel-jlavialle <jlavialle@degetel.com>
2014-11-24 16:39:53 +0100

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,30 @@
-require 'mina/bundler'
-require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
-
 # Basic settings:
 #   domain       - The hostname to SSH to.
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
-set :branch, 'master'
-
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
+case ENV['to']
+  when 'production'
+    set :domain, 'www.foo.bar'
+    set :deploy_to, '/home/foo'
+    set :repository, '/home/git/repositories/foo.git'
+    set :branch, 'deploy'
+  else
+    set :domain, '5.135.191.121'
+    set :deploy_to, '/var/www/Mina'
+    set :repository, 'git@github.com:degetel-jlavialle/test.git'
+    set :branch, 'master'
+end
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['sites/default/settings.php', 'cache', 'sites/default/files']
 
 # Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+set :user, 'root'    # Username in the server to SSH to.
+#set :port, '22'     # SSH port number.
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
@@ -42,33 +41,99 @@ end
 # For Rails apps, we'll make some of the shared paths that are shared between
 # all releases.
 task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+  queue! %[mkdir -p "#{deploy_to}/shared/sites/default/files"]
+  queue! %[chmod g+rwx,u+rwx "#{deploy_to}/shared/sites/default/files"]
 
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+  queue! %[mkdir -p "#{deploy_to}/shared/sites/default/files_private"]
+  queue! %[chmod g+rwx,u+rwx "#{deploy_to}/shared/sites/default/files_private"]
 
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
+  queue! %[mkdir -p "#{deploy_to}/shared/sites/default/tmp"]
+  queue! %[chmod g+rwx,u+rwx "#{deploy_to}/shared/sites/default/tmp"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/sites/default/sql_dumps"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/sites/default/sql_dumps"]
+
+  queue! %[touch "#{deploy_to}/shared/sites/default/settings.php"]
+  queue  %[echo "Be sure to edit."]
 end
 
-desc "Deploys the current version to the server."
-task :deploy => :environment do
+desc "Deploys the current version to the server for the first deployment."
+task :first_deploy => :environment do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
-    invoke :'deploy:cleanup'
+  end
+end
+
+desc "Deploys the current version to the server, and backup database."
+task :deploy => :environment do
+  deploy do
+    # Breek: Specific Drupal tasks
+    invoke :'site_offline'
+    invoke :'backup_database'
+
+    # Common Mina tasks
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+
+    # Breek: Specific Drupal tasks
+    invoke :'backup_database'
+    invoke :'database_updates'
+    invoke :'run_cron'
+    invoke :'clear_caches'
+    invoke :'clear_advagg'
 
     to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      invoke :'site_online'
     end
   end
+end
+
+desc "Site is put in maintenance mode"
+task :site_offline do
+  queue %[echo "-----> Site is put in maintenance mode."]
+  queue %[drush --root=#{deploy_to}/test --uri=http://#{domain} variable-set --yes --pipe --always-set maintenance_mode 1]
+end
+
+desc "Site is put in live"
+task :site_online do
+  queue %[echo "-----> Site is put in live."]
+  queue %[drush --root=#{deploy_to}/test --uri=http://#{domain} variable-set --yes --pipe --always-set maintenance_mode 0]
+end
+
+desc "Clearing Drupal caches"
+task :clear_caches do
+  queue %[echo "-----> Clearing Drupal caches."]
+  queue %[drush --root=#{deploy_to}/test --uri=http://#{domain} --yes --pipe cache-clear all]
+end
+
+desc "Clear advagg directories"
+task :clear_advagg do
+  queue %[echo "-----> Clearing AdvAgg directories."]
+  queue %[rm -Rf #{deploy_to}/shared/sites/default/files/advagg_css/*]
+  queue %[rm -Rf #{deploy_to}/shared/sites/default/files/advagg_js/*]
+end
+
+desc "Backuping up database using Drush"
+task :backup_database do
+  queue %[echo "-----> Backuping up database using Drush."]
+  t = Time.now.strftime("%Y-%m-%dT%H-%M-%S")
+  queue %[drush --root=#{deploy_to}/test --uri=http://#{domain} --yes --pipe sql-dump --gzip --result-file=#{deploy_to}/shared/sites/default/sql_dumps/#{domain}-#{t}.sql]
+  queue %[echo "-----> #{deploy_to}/shared/sites/default/sql_dumps/#{domain}-#{t}.sql"]
+end
+
+desc "Executing database updates"
+task :database_updates do
+  queue %[echo "-----> Executing database updates."]
+  queue %[drush --root=#{deploy_to}/test --uri=http://#{domain} --yes --pipe updatedb]
+end
+
+desc "Running cron"
+task :run_cron do
+  queue %[echo "-----> Running cron."]
+  queue %[drush --root=#{deploy_to}/test --uri=http://#{domain} --yes --pipe cron]
 end
 
 # For help in making your deploy script, see the Mina documentation:
